62d6518b44d4121b6a1af85af45043b6aa4a179b
Aaditya Shukla <itsaadityashukla@gmail.com>
2025-06-25 14:16:17 +0000

    Implemented create action

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
-import Dashboard from "./components/Dashboard";
+import CreateTask from "components/Tasks/Create";
+import Dashboard from "components/Dashboard";
 const App = () => {
   return (
     <Router>
       <Switch>
         <Route exact path="/" render={() => <div><h1>home</h1></div>} />
-        <Route exact path="/about" render={() => <div>About</div>} />
-         <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/tasks/create" component={CreateTask} />
+        <Route exact path="/dashboard" component={Dashboard} />
       </Switch>
     </Router>
   );
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,6 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+  def errors_to_sentence
+    errors.full_messages.to_sentence
+  end
 end
