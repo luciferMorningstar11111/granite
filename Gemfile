3d0c57ca75d77f657a5c0b4ff8c48b8f33ebcd5c
Aaditya Shukla <itsaadityashukla@gmail.com>
2025-07-03 15:54:37 +0000

    Integrated factory-bot and faker

--- Gemfile
+++ Gemfile
@@ -64,3 +64,11 @@ gem 'react-rails', "~> 2.7.1"
 gem "pry-byebug"
 gem 'bcrypt', '~> 3.1.13'
 gem 'simplecov', require: false, group: :test
+
+group :development, :test do
+  # Rails integration for factory_bot, a replacement for fixtures
+  gem 'factory_bot_rails'
+
+  # For auto-generating demo data
+  gem 'faker'
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -14,9 +14,6 @@ end
 
 enable_test_coverage if ENV['COVERAGE']
 
-ENV["RAILS_ENV"] ||= "test"
-require_relative "../config/environment"
-require "rails/test_help"
 
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
@@ -24,6 +21,8 @@ require "rails/test_help"
 
 module ActiveSupport
   class TestCase
+      include FactoryBot::Syntax::Methods
+
     # Run tests in parallel with specified workers
       parallelize(workers: :number_of_processors) unless ENV['COVERAGE']
 
