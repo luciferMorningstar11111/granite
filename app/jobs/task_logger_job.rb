0fe464e67f209f90cdbef2e94b31c3a41e3ff288
Aaditya Shukla <itsaadityashukla@gmail.com>
2025-07-15 14:20:01 +0000

    Added service to customize task logger message.

--- app/jobs/task_logger_job.rb
+++ app/jobs/task_logger_job.rb
@@ -1,11 +1,11 @@
 class TaskLoggerJob
   include Sidekiq::Worker
 
- def perform(task_id)
-  task = Task.find(task_id)
-  message = "A task was created with the following title: #{task.title}"
+  def perform(task_id)
+    task = Task.find(task_id)
+    message = LoggerMessageBuilderService.new(task).process!
 
-  log = Log.create! task_id: task.id, message:
-  puts log.message
-end
+    log = Log.create!(task_id: task.id, message:)
+    puts log.message
+  end
 end
